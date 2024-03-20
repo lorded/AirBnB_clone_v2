1be9c3197744c5da03e13bedd7afc7410f4be4a8
lorded <mynadishon@gmail.com>
2024-03-21 00:55:03 +0300

    updates

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,12 @@
-- MySQL server for this  project.
+-- Create MySQL database for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create MySQL user for the project
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant select privileges on performance_schema to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
